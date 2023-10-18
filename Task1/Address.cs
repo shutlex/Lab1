2a37e535886f9ef96a5a4143f255404e5a01c0f5
Oleksandr <sprokhorenko0609@gmail.com>
2023-10-18 23:20:54 +0300

    first

---
+++ Task1/Address.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Address
+{
+
+
+    private string index, country, city, street, house, apartment;
+    public string Index
+    {
+        get { return index; }
+        set { index = value; }
+    }
+    public string Country
+    {
+        get { return country; }
+        set { country = value; }
+    }
+    public string City
+    {
+        get { return city; }
+        set { city = value; }
+    }
+    public string Street
+    {
+        get { return street; }
+        set { street = value; }
+    }
+    public string House
+    {
+        get { return house; }
+        set { house = value; }
+    }
+    public string Apartment
+    {
+        get { return apartment; }
+        set { apartment = value; }
+    }
+}
+
---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.Title = "Прохоренко Олександр";
+
+        Address address = new Address();
+        address.Index = "04053";
+        address.Country = "Ukraine";
+        address.City = "Kyiv";
+        address.Street = "Levka Lukyanenko";
+        address.House = "13B";
+        address.Apartment = "521";
+        Console.WriteLine("Поштова адреса");
+        Console.WriteLine($"Індекс: {address.Index}");
+        Console.WriteLine($"Країна: {address.Country}");
+        Console.WriteLine($"Місто: {address.City}");
+        Console.WriteLine($"Вулиця: {address.Street}");
+        Console.WriteLine($"Будинок: {address.House}");
+        Console.WriteLine($"Квартира: {address.Apartment}");
+
+
+    }
+}
